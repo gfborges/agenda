88ab685981b9e47a1c66e01222cf734b74eb9710
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-03-18 23:38:26 -0300

    Melhorando o menu de opções

--- src/br/com/negocio/Menu.java
+++ src/br/com/negocio/Menu.java
@@ -2,18 +2,17 @@ package br.com.negocio;
 
 public class Menu {
 	public static void menu() {
-		System.out.println("\n~~~~~~~~~~~~~~~~~~~");
-		System.out.println("  MENU PRINCIPAL");
-		System.out.println("~~~~~~~~~~~~~~~~~~~");
-		System.out.println("1. Cadastrar cliente");
-		System.out.println("2. Editar cliente");
-		System.out.println("3. Remover cliente");
-		System.out.println("4. Buscar cliente");
-		System.out.println("5. Listar clientes");
-		System.out.println("6. Relatório de clientes");
-		System.out.println("7. Exportar clientes");
-		System.out.println("8. Importar clientes");
-		System.out.println("0. Sair");
+		System.out.println("\n+------- MENU PRINCIPAL -------+");
+		System.out.println("| [1] Cadastrar cliente        |");
+		System.out.println("| [2] Editar cliente           |");
+		System.out.println("| [3] Remover cliente          |");
+		System.out.println("| [4] Buscar cliente           |");
+		System.out.println("| [5] Listar clientes          |");
+		System.out.println("| [6] Relatório de clientes    |");
+		System.out.println("| [7] Exportar clientes        |");
+		System.out.println("| [8] Importar clientes        |");
+		System.out.println("| [0] Sair                     |");
+		System.out.println("+------------------------------+");
 		System.out.print("Insira uma opção: ");
 	}
 }
